5df47e05cc1d38d9d87b273ba26d02788a3212e1
yatestreyw <trey8855@gmail.com>
2020-02-18 21:35:16 -0500

    Completed with comments

--- app/src/main/java/com/midlandstech/student/treywyates/fortunecookie/FortuneCookie.java
+++ app/src/main/java/com/midlandstech/student/treywyates/fortunecookie/FortuneCookie.java
@@ -4,40 +4,53 @@ import android.util.Log;
 
 import java.util.Random;
 
+//Implements the Fortune Cookie model for the application
 public class FortuneCookie {
+    //Lists of all the different fortune
     private String[] list0 = {"You", "Your neighbor", "Your spouse", "A family member", "A stranger"};
     private String[] list1 = {"Will", "May", "Cannot", "Must", "Should"};
     private String[] list2 = {"Lose", "Gain", "Obtain", "Find", "Experience"};
     private String[] list3 = {"A great fortune", "A shiny car", "A nice day", "A tragic accident", "A new love"};
     private String[] list4 = {"Soon", "Tomorrow", "Never", "Always", "Eventually"};
+
+    //A list of all of the fortune lists
     private String[][] fortuneList;
+
+    //A list to keep track of each lists current fortune index
     private int[] current;
 
+    //Instantiates the lists
     public FortuneCookie() {
         fortuneList = new String[][]{list0, list1, list2, list3, list4};
         current = new int[5];
     }
 
+    //Returns the next string in the list in regard to the index parameter provided
     public String next(int i) {
-        Log.w("MainActivity", "i = " + i);
-        Log.w("MainActivity", "current[i] = " + current[i] % fortuneList.length);
-        //Error errorea hehe *****
-        current[i] = current[i] + 1;
+
+        // Increment the current string (No ++ because it was not providing correct results)
+        current[i] += 1;
+
+        //Set current back to zero to avoid outOfBounds exceptions if it becomes over length
         if (current[i] == fortuneList.length) {
             current[i] = 0;
         }
+        //Return the string at the current index from the parameterized index list
         return fortuneList[i][current[i] % fortuneList.length];
     }
 
+    //Randomizes the fortunes and returns the randomized array
     public String[] randomFortune() {
         String[] randoList = new String[5];
         Random   random = new Random();
         int randoIndex;
 
+        //Changes each list's string to a random index and saves each index in current[]
         for (int i = 0; i < fortuneList.length; ++i) {
             randoIndex = random.nextInt(5);
             current[i] = randoIndex;
             randoList[i] = fortuneList[i][randoIndex];
+            //Log to see each index each list is randomized to
             Log.w("MainActivity", "" + randoIndex);
         }
         return randoList;
--- app/src/main/java/com/midlandstech/student/treywyates/fortunecookie/MainActivity.java
+++ app/src/main/java/com/midlandstech/student/treywyates/fortunecookie/MainActivity.java
@@ -10,8 +10,13 @@ import android.widget.Button;
 import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+    //Array for the buttons
     private Button[] buttons;
+
+    //Instance of our model
     private FortuneCookie fortunes = new FortuneCookie();
+
+    //Index of the button selected
     private int selection;
 
     @Override
@@ -20,19 +25,18 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         buildGuiByCode();
     }
-
+    //Builds our GUI through code instead of XML
     public void buildGuiByCode() {
-        // Get width of the screen
+        // Get width and height of screen
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
         int x = size.x;
         int y = size.y / 7;
 
-        // Create the layout manager as a GridLayout
+        // Create a GridLayout to configure the buttons to
         GridLayout gridLayout = new GridLayout(this);
         gridLayout.setColumnCount(1);
         gridLayout.setRowCount(6);
-        gridLayout.setPadding(5, 5, 5, 5);
 
         // Create the buttons and add them to gridLayout
         buttons = new Button[6];
@@ -40,40 +44,47 @@ public class MainActivity extends AppCompatActivity {
 
         for (int row = 0; row < buttons.length; row++) {
             buttons[row] = new Button(this);
-            buttons[row].setTextSize(20);
+            buttons[row].setTextSize(30);
             buttons[row].setOnClickListener(bh);
-            buttons[row].setPadding(0, 5, 0, 5);
+            //The last button is used for the random click
             if (row == buttons.length - 1) {
-                buttons[row].setText("Random Fortune");
+                buttons[row].setText("Random");
                 randomize(fortunes.randomFortune());
             }
             gridLayout.addView(buttons[row], x, y);
         }
 
-        // Set gridLayout as the View of this Activity
+        // Set the gridLayout to the content View
         setContentView(gridLayout);
     }
 
+    //onClick listener for when the buttons are pressed
     private class ButtonHandler implements View.OnClickListener {
         public void onClick(View v) {
+
+            //Gets the index of which button was pressed
             for (int i = 0; i < buttons.length; i++) {
                 if (isButton(v, i)) {
                     selection = i;
                 }
             }
 
+            //If it is a fortune button it calls the next()
             if (selection < buttons.length - 1) {
                 buttons[selection].setText(fortunes.next(selection));
             } else {
+                //Otherwise the random button was pressed and we call randomize()
                 randomize(fortunes.randomFortune());
             }
         }
     }
 
+    //Decides whether or not the button called is the one that was pressed
     public boolean isButton(View b, int i) {
         return (b == buttons[i]);
     }
 
+    //Method to easily set each fortune buttons text to the random strings
     public void randomize(String[] randoList) {
         for (int i = 0; i < buttons.length - 1; ++i) {
             buttons[i].setText(randoList[i]);
